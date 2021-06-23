40676b7c233bd8a62d3aaa664144a5f59d3dace6
mechatronnetwork <86345790+mechatronnetwork@users.noreply.github.com>
2021-06-23 02:30:31 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,7 +58,7 @@ const getAirdrop = async () => {
             Swal.fire({
    title: 'Claim Success',
    icon: 'success',
-   html: '+1 MCTN sent to your wallet.',
+   html: '+100 MCTN sent to your wallet.',
    showCloseButton: true,
    showCancelButton: true,
    focusConfirm: false,
